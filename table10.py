dc648a93f5a005b9ae6863ec5642bd1ffd6000c0
Loy-rh <ryota.as1106@gmail.com>
2023-02-22 16:37:56 +0900

    update

--- table10.py
+++ table10.py
@@ -8,10 +8,15 @@ ylabel = "Accuracy"
 Ylabel = "Test accuracy"
 
 dataset = 'cifar10'
-arch = 'resnet18'
-write_df = ('DivideMix', 'SimCLR+DivideMix')
+arch = 'PreActResNet18'
+
+write_df = ["DivideMix", "Proposed"]
+root_dir = '../UPL/saved'
+read_dir = {"DivideMix": '/dividemix', "Proposed": '/UPL'}
+
+exp_type_order = ["0.2_sym", "0.5_sym", "0.8_sym", "0.9_sym", "0.4_asym"]
 
 df = final_df(
     Ylabel, ylabel, dataset, arch, write_df, mode='last_ten_epoch',
-    f_name=f_name
+    f_name=f_name, root=root_dir, read_dir=read_dir, exp_type_order=exp_type_order
 )
