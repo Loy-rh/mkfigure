c5d9bc62d3e3ead32cf7385a691f7b9877e3938b
Loy-rh <ryota.as1106@gmail.com>
2023-04-14 19:30:33 +0900

    update

--- table100.py
+++ table100.py
@@ -15,9 +15,12 @@ arch = 'PreActResNet18'
 # write_df = ["DivideMix", "Proposed"]
 # root_dir = '../UPL/saved'
 # read_dir = {"DivideMix": '/dividemix', "Proposed": '/UPL'}
-write_df = ["CRAS", "wELR"]
+# write_df = ["CRAS", "wELR"]
+# root_dir = '../saved'
+# read_dir = {"CRAS": '/CRAS', "wELR": '/wELR'}
+write_df = ["DivideMix", "CRAS"]
 root_dir = '../saved'
-read_dir = {"CRAS": '/CRAS', "wELR": '/wELR'}
+read_dir = {"DivideMix": '/DivideMix', "CRAS": '/CRAS'}
 
 exp_type_order = ["0.2_sym", "0.5_sym", "0.8_sym", "0.9_sym", "0.4_asym"]
 # exp_type_order = ["0.2_sym", "0.5_sym", "0.8_sym", "0.9_sym"]
