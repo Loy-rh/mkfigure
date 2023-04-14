77b3a8488d012f62a6c0115e0f6a78f897d65b8b
Loy-rh <ryota.as1106@gmail.com>
2023-04-14 19:29:45 +0900

    update

--- dir_setting.py
+++ dir_setting.py
@@ -115,6 +115,7 @@ def final_df(Ylabel, ylabel, dataset, arch, hue_order, mode='figure', opt='avera
             for exp_type, exp_type2 in zip(exp_type_order, exp_type_order2):
                 base_addr = "{}{}/log/{}_{}_{}_{}/*".format(
                     root, read_dir[method], dataset, method, arch, exp_type2)
+                print(base_addr)
                 process(method, base_addr, exp_type)
             if mode == 'last_ten_epoch':
                 mk_csv(df, method, f_name, arch, ylabel)
