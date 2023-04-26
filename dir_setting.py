2cca14dd217b6178c41f57d362b41b203df8b2b5
Loy-rh <ryota.as1106@gmail.com>
2023-04-26 14:41:56 +0900

    update

--- dir_setting.py
+++ dir_setting.py
@@ -132,6 +132,13 @@ def final_df(Ylabel, ylabel, dataset, arch, hue_order, mode='figure', opt='avera
                 process(method, base_addr, exp_type)
             if mode == 'last_ten_epoch':
                 mk_csv(df, method, f_name, arch, ylabel)
+        elif method == 'UPL' or 'UPLplus':
+            for exp_type, exp_type2 in zip(exp_type_order, exp_type_order2):
+                base_addr = "{}{}/log/{}_{}_{}_{}/*".format(
+                    root, read_dir[method], dataset, method, arch, exp_type2)
+                process(method, base_addr, exp_type)
+            if mode == 'last_ten_epoch':
+                mk_csv(df, method, f_name, arch, ylabel)
         else:
             for exp_type, exp_type2 in zip(exp_type_order, exp_type_order2):
                 base_addr = "{}{}/log/{}_CRAS_{}_{}/*".format(
--- figure100.py
+++ figure100.py
@@ -18,12 +18,11 @@ dataset = 'cifar100'
 arch = 'PreActResNet18'
 # col_wrap = 2
 col_wrap = 4  # 2x2
-root_dir = '../saved'
+root_dir = '../UPLplus/saved'
 # write_df = ["DivideMix", "CRAS"]
 # read_dir = {"DivideMix": '/DivideMix', "CRAS": '/CRAS'}
-write_df = ["DivideMix", "CRAS w/o CR", "CRAS w/o AWL", "CRAS"]
-read_dir = {"DivideMix": '/DivideMix',
-            "CRAS w/o CR": "/wo_CELR", "CRAS w/o AWL": "/wo_weight", "CRAS": '/CRAS'}
+write_df = ["UPL", "UPLplus"]
+read_dir = {"UPL": "/UPL", "UPLplus": "/UPL"}
 
 
 exp_type_order = ["0.2_sym", "0.5_sym", "0.8_sym", "0.9_sym"]
