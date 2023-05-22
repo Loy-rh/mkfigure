df0e4de450160a8b8059909ef1c1665a2e0e31bf
Loy-rh <ryota.as1106@gmail.com>
2023-05-22 11:48:58 +0900

    update

--- make_figure.py
+++ make_figure.py
@@ -56,7 +56,7 @@ def mk_fig(
             #     g.set(yticks=[0, 10, 20, 30, 40, 50, 60, 70, 80])
             figsize = (16, 4)
         elif col_wrap == 2:
-            if legend
+            if legend:
                 g.fig.axes[0].legend(handles, labels, bbox_to_anchor=(1, 1), loc='lower center',
                                      ncol=len(hue_order), frameon=True, borderaxespad=3)
             g.set(xticks=[0, 50, 100, 150, 200, 250, 300])
