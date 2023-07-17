4ced40b4885f0322939d73305d111285f1c0c795
Loy-rh <ryota.as1106@gmail.com>
2023-07-17 16:35:20 +0900

    update

--- make_figure.py
+++ make_figure.py
@@ -40,7 +40,7 @@ def mk_fig(
         g = g.map(sns.lineplot, 'Epoch', Ylabel, **kws).add_legend()
         g.set_titles("{col_name}")
         g.fig.subplots_adjust(wspace=0.1)
-        g.set(ylim=ylim)
+        g.set(ylim=ylim, font_scale=2.)
 
         # the process required to create a legend that is similar to the original paper
         lg = g.fig.legends[0]
@@ -74,7 +74,6 @@ def mk_fig(
         else:
             raise NotImplementedError
 
-    g.setp(font_size=24)
     folder = f"./result/{f_name}/{dt_d + dt_s}"
     if not os.path.exists(folder):
         os.system(f"mkdir -p {folder}")
