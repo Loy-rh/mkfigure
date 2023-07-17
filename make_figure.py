20ce5eb21288539ff0df8cb11763dd7dad14a386
Loy-rh <ryota.as1106@gmail.com>
2023-07-17 17:25:04 +0900

    update

--- make_figure.py
+++ make_figure.py
@@ -35,7 +35,7 @@ def mk_fig(
         kws = dict(linewidth=.8)
         g = sns.FacetGrid(
             df, col='Noise Type', hue='Method', hue_order=hue_order,
-            palette=palette, col_wrap=col_wrap, height=5
+            palette=palette, col_wrap=col_wrap, height=8
         )
         g = g.map(sns.lineplot, 'Epoch', Ylabel, **kws).add_legend()
         g.set_titles("{col_name}")
@@ -60,7 +60,7 @@ def mk_fig(
                 g.set(xticks=[0, 20, 40, 60, 80, 100])
             # if dataset == 'cifar100':
             #     g.set(yticks=[0, 10, 20, 30, 40, 50, 60, 70, 80])
-            # figsize = (16, 4)
+            figsize = (16, 4)
         elif col_wrap == 2:
             if legend:
                 g.fig.axes[0].legend(handles, labels, bbox_to_anchor=(1, 1), loc='lower center',
@@ -70,7 +70,7 @@ def mk_fig(
             else:
                 g.set(xticks=[0, 20, 40, 60, 80, 100])
             # g.set(yticks=[10, 20, 30, 40, 50, 60, 70])
-            # figsize = (12, 8)
+            figsize = (12, 8)
         else:
             raise NotImplementedError
 
@@ -78,7 +78,7 @@ def mk_fig(
     if not os.path.exists(folder):
         os.system(f"mkdir -p {folder}")
     fig = plt.gcf()
-    # fig.set_size_inches(figsize)
+    fig.set_size_inches(figsize)
 
     # save figure
     fig.savefig(f"{folder}/figure{dataset}_{text}.png", bbox_inches='tight', pad_inches=0.1)
