b3767fc08d3b6b8ea79e4e2c6f99b6c3a15bf884
Loy-rh <ryota.as1106@gmail.com>
2023-07-18 11:09:38 +0900

    update

---
+++ beta.py
@@ -0,0 +1,17 @@
+from scipy.stats import beta
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+plt.rcParams['font.family'] = 'Times New Roman'
+x = np.linspace(0, 1, 100)
+alpha = 0.5
+y = beta.pdf(x, alpha, alpha)
+plt.plot(x, y)
+
+fig = plt.gcf()
+pp = PdfPages("fig/beta_{}.pdf".format(alpha))
+pp.savefig(fig)
+pp.close()
+pp.clf()
