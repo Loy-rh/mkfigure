80d9068502ffd901430533f320e518f4ec9c3d2c
Loy-rh <ryota.as1106@gmail.com>
2023-08-09 14:23:51 +0900

    chenge linewidth

--- beta.py
+++ beta.py
@@ -8,7 +8,7 @@ plt.rcParams['font.family'] = 'Times New Roman'
 x = np.linspace(0, 1, 100)
 alpha = 0.5
 y = beta.pdf(x, alpha, alpha)
-plt.plot(x, y)
+plt.plot(x, y, linewidth=3)
 
 fig = plt.gcf()
 pp = PdfPages("fig/beta_{}.pdf".format(alpha))
