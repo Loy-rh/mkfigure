4448bbfceab370ac087835a96333687cdca1d6a3
Loy-rh <ryota.as1106@gmail.com>
2023-08-10 10:34:32 +0900

    update

--- figure100.py
+++ figure100.py
@@ -22,8 +22,8 @@ Ylabel = 'Rate of labeled samples'
 
 dataset = 'cifar100'
 arch = 'PreActResNet18'
-col_wrap = 2
-# col_wrap = 4  # 2x2
+# col_wrap = 2
+col_wrap = 4  # 2x2
 root_dir = './saved'
 # write_df = ["DivideMix", "CRAS"]
 # read_dir = {"DivideMix": '/DivideMix', "CRAS": '/CRAS'}
